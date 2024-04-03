d868a1fd9714bda1e864cbc27ca45b046e8f0de4
HWenSu <trista44488@gmail.com>
2024-04-03 16:05:31 +0800

    Reverse to MVC version.

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
+const DISPLAY_STATE = {
+  ListDisplay: 'ListDisplay',
+  CardDisplay: 'CardDisplay'
+}
+
 const BASE_URL = 'https://webdev.alphacamp.io'
 const INDEX_URL = BASE_URL + '/api/movies/'
 const POSTER_URL = BASE_URL + '/posters/'
 const MOVIES_PER_PAGE = 12
-
-const movies = []
-let filteredMovies = []
 
 const dataPanel = document.querySelector('#data-panel')
 const searchForm = document.querySelector('#search-form')
 const searchInput = document.querySelector('#search-input')
 const paginator = document.querySelector('#paginator')
 
-//產生所有電影資料HTML函式
-function renderMovieList (data){
-  let rawHTML = ''
-  data.forEach((item) => {
+const model = {
+  movies: [],
+  filteredMovies: []
+}
+
+const view = {
+  //產生所有電影資料HTML函式
+  renderMovieList (data){
+    let rawHTML = ''
+    data.forEach((item) => {
     //title, image
-    rawHTML += `
+      rawHTML += `
     <div class="col-sm-3">
   <div class="mb-2">
     <div class="card">
@@ -34,12 +42,38 @@ function renderMovieList (data){
     </div>
   </div>
   `
+    })
+    dataPanel.innerHTML = rawHTML
+  },
+
+  //產生所有電影資料HTML函式
+  renderMovieList (data){
+    let rawHTML = ''
+    data.forEach((item) => {
+    //title, image
+      rawHTML += `
+       <div class="col-sm-3">
+        <div class="mb-2">
+        <div class="card">
+          <img src="${
+        POSTER_URL + item.image
+        }" class="card-img-top" alt="Movie Poster">
+        <div class="card-body">
+        <h5 class="card-title"> ${item.title} </h5>
+        </div>
+      <div class="card-footer">
+        <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${item.id}">More</button>
+        <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
   })
   dataPanel.innerHTML = rawHTML
-}
-
+},
 // showMovieModal 顯示單筆電影資料函式
-function showMovieModal(id) {
+showMovieModal(id) {
   const modalTitle = document.querySelector('#movie-modal-title')
   const modalImage = document.querySelector('#movie-modal-image')
   const modalDate = document.querySelector('#movie-modal-date')
@@ -52,13 +86,12 @@ function showMovieModal(id) {
     modalImage.innerHTML = `<img src="${POSTER_URL + data.image
       }" alt="movie-poster" class="img-fluid">`
   })
-}
-
+},
 //收藏電影函式
-function addToFavorite (id) {
+addToFavorite (id) {
 
   const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
-  const movie = movies.find((movie) => movie.id === id)
+  const movie = model.movies.find((movie) => movie.id === id)
   
   if (list.some((movie) => movie.id === id)){
     return alert('電影已在收藏清單中！')
@@ -68,9 +101,9 @@ function addToFavorite (id) {
   localStorage.setItem('favoriteMovies', JSON.stringify(list))
 
   console.log(list)
-}
+},
 // 建立分頁器
-function renderPaginator(amount) {
+renderPaginator(amount) {
   //計算總頁數
   const numberOfPages = Math.ceil(amount / MOVIES_PER_PAGE)
   //製作template
@@ -83,22 +116,47 @@ function renderPaginator(amount) {
     </li>`
   }
   paginator.innerHTML = rawHTML
-}
-
-function getMoviesByPage(page) {
-  const data = filteredMovies.length? filteredMovies : movies
+},
+getMoviesByPage(page) {
+  const data = model.filteredMovies.length? model.filteredMovies : model.movies
   //計算起始Index
   const startIndex = (page - 1) * MOVIES_PER_PAGE
   //回傳切割後的新陣列
   return data.slice(startIndex, startIndex + MOVIES_PER_PAGE)
 }
 
+}
+
+const controller = {
+  currentState: DISPLAY_STATE.CardDisplay,
+  generateCards(page){
+    view.renderMovieList(view.getMoviesByPage(page))
+  },
+  generatePaginator(amount){
+    view.renderPaginator(amount)
+  }
+
+}
+
+
+
+//串接API
+axios
+.get(INDEX_URL) // 修改這裡
+.then((response) => {
+  model.movies.push(...response.data.results)
+  controller.generatePaginator(model.movies.length)
+  controller.generateCards(1)
+  
+})
+.catch((err) => console.log(err))
+
 //監聽 data panel
 dataPanel.addEventListener('click', function onPanelClicked(event) {
   if (event.target.matches('.btn-show-movie')) {
-    showMovieModal(Number(event.target.dataset.id))  // 修改這裡
+    view.showMovieModal(Number(event.target.dataset.id))  // 修改這裡
   } else if (event.target.matches('.btn-add-favorite')){
-    addToFavorite(Number(event.target.dataset.id))
+    view.addToFavorite(Number(event.target.dataset.id))
   }
 })
 
@@ -106,27 +164,18 @@ dataPanel.addEventListener('click', function onPanelClicked(event) {
 searchForm.addEventListener('submit', function onSearchFormSubmitted(event){
   event.preventDefault()
   const keyword = searchInput.value.trim().toLowerCase()
-  
-  
-  // if(!keyword.length){
-  //   return alert('please enter a valid string')
-  // }
 
-  filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(keyword))
+  model.filteredMovies = model.movies.filter(movie => movie.title.toLowerCase().includes(keyword))
 
-  if(filteredMovies.length === 0){
+  if(model.filteredMovies.length === 0){
     alert('Cannot find movies with keyword:' + keyword)
   }
 
-  // for(const movie of movies){
-  //   if (movie.title.toLowerCase().includes(keyword)){
-  //     filteredMovies.push(movie)
-  //   }
-  // }
   //重置分頁器
-  renderPaginator(filteredMovies.length)
+  controller.generatePaginator(model.filteredMovies.length)
+  // view.renderPaginator(filteredMovies.length)
   //預設第一頁的搜尋結果
-  renderMovieList(getMoviesByPage(1))
+  controller.generateCards(1)
 })
 
 // listen to paginator
@@ -136,18 +185,5 @@ paginator.addEventListener('click', function onPaginatorClicked(event) {
   //透過dataset取得被點擊的頁數
   const page = Number(event.target.dataset.page)
   //更新畫面
-  renderMovieList(getMoviesByPage(page))
+  controller.generateCards(page)
 })
-
-
-//串接API
-axios
-  .get(INDEX_URL) // 修改這裡
-  .then((response) => {
-    movies.push(...response.data.results)
-    renderPaginator(movies.length)
-    renderMovieList(getMoviesByPage(1))
-  })
-  .catch((err) => console.log(err))
-
-//共用資料
