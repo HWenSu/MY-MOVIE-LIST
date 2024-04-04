37b4c27f408c0aee6ab84c780ae5d46dff3f61e1
HWenSu <trista44488@gmail.com>
2024-04-04 23:49:37 +0800

    Add list and card style display.

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const dataPanel = document.querySelector('#data-panel')
 const searchForm = document.querySelector('#search-form')
 const searchInput = document.querySelector('#search-input')
 const paginator = document.querySelector('#paginator')
+const displayStyleBtn = document.querySelector('#style-display-btn')
+
+let currentPage = 1
 
 const model = {
   movies: [],
@@ -46,28 +49,20 @@ const view = {
     dataPanel.innerHTML = rawHTML
   },
 
-  //產生所有電影資料HTML函式
-  renderMovieList (data){
+  //條列式電影資料
+  renderMovieListByList (data){
     let rawHTML = ''
     data.forEach((item) => {
     //title, image
       rawHTML += `
-       <div class="col-sm-3">
-        <div class="mb-2">
-        <div class="card">
-          <img src="${
-        POSTER_URL + item.image
-        }" class="card-img-top" alt="Movie Poster">
-        <div class="card-body">
-        <h5 class="card-title"> ${item.title} </h5>
-        </div>
-      <div class="card-footer">
+      <ul class="list-group list-group-horizontal-xxl ">
+        <li class="list-group-item list ">${item.title}
+        <div>
         <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${item.id}">More</button>
-        <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
+        <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>     
         </div>
-      </div>
-    </div>
-  </div>
+        </li>
+      </ul>
   `
   })
   dataPanel.innerHTML = rawHTML
@@ -129,39 +124,38 @@ getMoviesByPage(page) {
 
 const controller = {
   currentState: DISPLAY_STATE.CardDisplay,
-  generateCards(page){
-    view.renderMovieList(view.getMoviesByPage(page))
-  },
+  // generateCards(page){
+  //   view.renderMovieList(view.getMoviesByPage(page))
+  // },
   generatePaginator(amount){
     view.renderPaginator(amount)
+    
+  },
+  // generateList(page){
+  //   view.renderMovieListByList(view.getMoviesByPage(page))
+  // },
+  displayMoviesByStyle(page){
+    switch(this.currentState) {
+      case DISPLAY_STATE.CardDisplay:
+        view.renderMovieList(view.getMoviesByPage(page))
+        break
+      case DISPLAY_STATE.ListDisplay:
+        view.renderMovieListByList(view.getMoviesByPage(page))
+    } 
   }
-
 }
-
-
-
-//串接API
-axios
-.get(INDEX_URL) // 修改這裡
-.then((response) => {
-  model.movies.push(...response.data.results)
-  controller.generatePaginator(model.movies.length)
-  controller.generateCards(1)
-  
-})
-.catch((err) => console.log(err))
 
 //監聽 data panel
 dataPanel.addEventListener('click', function onPanelClicked(event) {
   if (event.target.matches('.btn-show-movie')) {
-    view.showMovieModal(Number(event.target.dataset.id))  // 修改這裡
+    view.showMovieModal(Number(event.target.dataset.id))
   } else if (event.target.matches('.btn-add-favorite')){
     view.addToFavorite(Number(event.target.dataset.id))
   }
 })
 
 //監聽表單提交事件
-searchForm.addEventListener('submit', function onSearchFormSubmitted(event){
+searchForm.addEventListener('submit', function onSearchFormSubmitted(event) {
   event.preventDefault()
   const keyword = searchInput.value.trim().toLowerCase()
 
@@ -185,5 +179,36 @@ paginator.addEventListener('click', function onPaginatorClicked(event) {
   //透過dataset取得被點擊的頁數
   const page = Number(event.target.dataset.page)
   //更新畫面
-  controller.generateCards(page)
+  controller.displayMoviesByStyle(page)
+  if(localStorage.getItem('getPage') !== 0) {
+    localStorage.removeItem('getPage')
+  }
+  localStorage.setItem('getPage', JSON.stringify(page))
 })
+
+
+
+//listen to displayStyleBtn
+displayStyleBtn.addEventListener('click', function onclick(event) {
+  currentPage = Number(localStorage.getItem('getPage'))
+  if (event.target.matches('.list-btn')) {
+    controller.currentState = DISPLAY_STATE.ListDisplay
+    controller.displayMoviesByStyle(currentPage)
+    console.log(controller.currentState)
+  } else if (event.target.matches('.card-btn')) {
+    controller.currentState = DISPLAY_STATE.CardDisplay
+    controller.displayMoviesByStyle(currentPage)
+    
+  }
+})
+
+
+//串接API
+axios
+.get(INDEX_URL) // 修改這裡
+.then((response) => {
+  model.movies.push(...response.data.results)
+  controller.generatePaginator(model.movies.length)
+  controller.displayMoviesByStyle(1)
+})
+.catch((err) => console.log(err))
